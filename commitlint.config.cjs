33a88e56f16ae8e3b6f9b6bf9865c675f43876cc
unknown <1328931645@qq.com>
2023-06-04 10:11:43 +0800

    fix: 变量定义

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     // 继承的规则
     extends: ['@commitlint/config-conventional'],
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,2 @@
 const add ='A'
-console.log(add)
+console.log(b)
